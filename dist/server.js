5234ad2cc130848a35382d9aa6eed179ddd1631c
dwthomas77 <david.thomas3@gmail.com>
2020-03-09 10:15:48 -0400

    basic page serving setup

--- dist/server.js
+++ dist/server.js
@@ -2,11 +2,16 @@
 
 var express = require('express');
 
+var path = require('path');
+
 var app = express();
+var router = express.Router();
 var port = 3000;
-app.get('/', function (req, res) {
-  return res.send('Hello World!');
-});
+router.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname + '/index.html')); //__dirname : It will resolve to your project folder.
+}); //add the router
+
+app.use('/', router);
 app.listen(port, function () {
   return console.log("Example app listening on port ".concat(port, "!"));
 });
--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 const express = require('express')
+const path = require('path');
+
 const app = express()
+const router = express.Router();
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+router.get('/',function(req,res){
+  res.sendFile(path.join(__dirname+'/index.html'));
+  //__dirname : It will resolve to your project folder.
+});
 
+//add the router
+app.use('/', router);
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
